0831b297b678506e41185c53dfa9cba126a18288
GPT <gpt123@gmyil.com>
2024-07-19 19:03:34 +0530

    exp added 1st

--- Exp.java
+++ Exp.java
@@ -2,8 +2,8 @@ class Exp
 {
 	public static void main(String s[])
 	{
-		int i=10,j=20,k;
-		k=i+j;
-		System.out.println("k="+k);
+		int i=10,j=20;
+		int sum=i-j;
+		System.out.println("sum="+sum);
 	}
 }
